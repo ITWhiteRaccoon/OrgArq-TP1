eda175db4e300333ded381463868f1bdf33ba988
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-10-03 01:23:11 -0300

    Create EduardoAndrade_ViniciusBerizzi.asm

---
+++ EduardoAndrade_ViniciusBerizzi.asm
@@ -0,0 +1,23 @@
+#Trabalho Pratico 1 para OrgArqI 2020/2
+#Eduardo Andrade, Vinicius Berizzi
+	.text
+	.globl main
+main:		
+		
+		
+carregaVetor:	
+		
+		
+encontraPadrao:	
+		
+		
+	.data
+strTamVet:	.asciiz "Informe o numero de dados a serem inseridos no vetor "
+strDadoVet:	.asciiz "Informe um dado a ser inserido no vetor "
+strQtdPadr:	.asciiz "Quantidade de padroes contabilizados: "
+strDados:	.asciiz "dados: "
+strPadrao:	.asciiz	"padrao: "
+intTamDados:	.word	0
+intTamPadrao:	.word	0
+vetDados:	.space	200
+vetPadrao:	.space	20
